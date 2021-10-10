3e95d7debd2fb448f589a77b27d01769f14db954
anirbandas10 <anirbandas1127@gmail.com>
2021-10-11 00:26:39 +0530

    Updates

--- PYSampleCode008.py
+++ PYSampleCode008.py
@@ -23,3 +23,27 @@ for i in range(104):
    t.forward(side)
    t.right(72) #Exterior angle of a pentagon is 72 degree
    side = side - 2
+
+#Python program to draw spider web in turtle programming
+t = turtle.Turtle()
+t.speed(0)
+ 
+#Code for building radical thread
+for i in range(6):
+  t.forward(150)
+  t.backward(150)
+  t.right(60)
+ 
+#Code for building spiral thread
+side = 150
+for i in range(15):
+  t.penup()
+  t.goto(0,0)
+  t.pendown()
+  t.setheading(0)
+  t.forward(side)
+  t.right(120)
+  for j in range(6):
+    t.forward(side)
+    t.right(60)
+  side = side - 10
