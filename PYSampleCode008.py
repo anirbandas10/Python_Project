e3aeecfeb189d9a5e03ca699fb0a972c85214718
anirbandas10 <anirbandas1127@gmail.com>
2021-10-11 00:16:55 +0530

    Updates

---
+++ PYSampleCode008.py
@@ -0,0 +1,25 @@
+#Python program to draw spiral star in turtle programming
+import turtle
+ 
+t = turtle.Turtle()
+side = 200
+for i in range(100):
+   t.forward(side)
+   t.right(144) #Exterior angle of a star is 144 degree
+   side = side - 2
+
+#Python program to draw spiral triangle in turtle programming 
+t = turtle.Turtle()
+side = 200
+for i in range(70):
+   t.forward(side)
+   t.right(120) #Exterior angle of a triangle is 120 degree
+   side = side - 3
+
+#Python program to draw spiral pentagon in turtle programming 
+t = turtle.Turtle()
+side = 200
+for i in range(104):
+   t.forward(side)
+   t.right(72) #Exterior angle of a pentagon is 72 degree
+   side = side - 2
